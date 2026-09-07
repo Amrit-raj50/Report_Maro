7d38c6d9106fcb02ffe0a7863337a85ff090f000
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-09-07 23:49:59 +0530

    authetication for user established

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-
+const authRoutes = require('./routes/auth.route');
 app.use(express.json());
 
-app.use('/' , (req,res) => {
-   res.send("hello there!");
-})
+// app.use('/' , (req,res) => {
+//    res.send("hello there!");
+// })
+
+app.use('/api/auth',authRoutes);
 
 app.use((req,res) => {
     res.status(404).json({msg : "route not found"});
