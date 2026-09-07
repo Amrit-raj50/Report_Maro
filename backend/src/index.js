329afd072e7b40d592d1bb2fa6408e5b6fcf401f
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-09-07 20:19:32 +0530

    mondoDB connected to the server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
 const app = require('./app.js');
 // const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+
+dotenv.config();
 
 const startServer = async() => {
     try{
+        await connectDB();
         app.listen(3000 , () => {
             console.log("server is running");
         })
