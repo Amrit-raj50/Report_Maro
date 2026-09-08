31615000a5c8c5d5cfd6c9e20f98d9309c974958
Amrit-raj50 <amrit.raj.cg@gmail.com>
2026-09-08 06:36:53 +0530

    feat(routes): register project management routes

---
+++ backend/src/routes/project.route.js
@@ -0,0 +1,25 @@
+// routes/project.routes.js
+const express = require('express');
+const { submitProposal, fundProject } = require('../controllers/project.controller');
+const authMiddleware = require('../middleware/auth.middleware');
+const rbacMiddleware = require('../middleware/rbac.middlewre');
+
+const router = express.Router();
+
+// University: Submit proposal
+router.post(
+  '/:id/proposal',
+  authMiddleware,
+  rbacMiddleware(['university']),
+  submitProposal
+);
+
+// Industry: Fund project
+router.put(
+  '/:id/fund',
+  authMiddleware,
+  rbacMiddleware(['industry']),
+  fundProject
+);
+
+module.exports = router;
